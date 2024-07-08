9452b53791864f7ebd0f1d77676a94af9d5fe221
Amol Shinde <shindeamol409@gmail.com>
2024-07-08 18:29:26 +0530

    fixed image issue Post.jsx

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -7,7 +7,8 @@ function Post({ _id, title, summary, content, cover, createdAt, author }) {
     <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={`${API_URL}/post` + cover} />
+          {/* <img src={`${API_URL}/post` + cover} /> */}
+          <img src={`${API_URL}/${cover}`} alt={title} />
         </Link>
       </div>
       <div className="texts">
