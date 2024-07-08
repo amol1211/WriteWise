f0c49f3d8e6cbd53e2ba245098c0591627b61717
Amol Shinde <shindeamol409@gmail.com>
2024-07-08 16:01:14 +0530

    fixed post issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,22 @@
-/* import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:4000",
         secure: false,
       },
     },
   },
   plugins: [react()],
-}); */
+});
 
 // vite.config.js
-import { defineConfig } from "vite";
+/* import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-});
+}); */
